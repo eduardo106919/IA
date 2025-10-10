8f62972e186594c89cf439623b8684ae5c8358f4
eduardo106919 <a106919@alunos.uminho.pt>
2025-10-10 19:17:41 +0100

    improved ficha 01

--- fichas/ficha-01/Graph.py
+++ fichas/ficha-01/Graph.py
@@ -1,214 +1,82 @@
+
 import math
-from queue import Queue
+import queue
 
-import networkx as nx  # biblioteca de tratamento de grafos necessária para desnhar graficamente o grafo
-import matplotlib.pyplot as plt  # idem
-
-from Node import Node
-
-
-# Constructor
-# Number of edges
-# Adjacancy matrix, adjacency list, list of edges
-
-# Methods for adding edges
-
-# Methods for removing edges
-
-# Methods for searching a graph
-# BFS, DFS, 
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Graph:
-    def __init__(self, directed=False):
-        self.m_nodes = []  
-        self.m_directed = directed
-        self.m_graph = {}  # dicionario para armazenar os nodos e arestas
 
-    #############
-    #    escrever o grafo como string
-    #############
+
+    def __init__(self, directed = False):
+        # dictionary that holds lists of pairs (node, weight)
+        self.edges = dict()
+        self.directed = directed
+
+
     def __str__(self):
         out = ""
-        for key in self.m_graph.keys():
-            out = out + "node" + str(key) + ": " + str(self.m_graph[key]) + "\n"
-            return out
-
-    ################################
-    #   encontrar nodo pelo nome
-    ################################
-
-    def get_node_by_name(self, name):
-        search_node = Node(name)
-        for node in self.m_nodes:
-            if node == search_node:
-                return node
-            else:
-                return None
-
-    ##############################3
-    #   imprimir arestas
-    ############################333333
-
-    def imprime_aresta(self):
-        listaA = ""
-        lista = self.m_graph.keys()
-        for nodo in lista:
-            for (nodo2, custo) in self.m_graph[nodo]:
-                listaA = listaA + nodo + " ->" + nodo2 + " custo:" + str(custo) + "\n"
-        return listaA
-
-    ################
-    #   adicionar   aresta no grafo
-    ######################
-
-    def add_edge(self, node1, node2, weight):
-        n1 = Node(node1)
-        n2 = Node(node2)
-        if n1 not in self.m_nodes:
-            n1_id = len(self.m_nodes)  # numeração sequencial
-            n1.setId(n1_id)
-            self.m_nodes.append(n1)
-            self.m_graph[node1] = []
-        
-
-        if n2 not in self.m_nodes:
-            n2_id = len(self.m_nodes)  # numeração sequencial
-            n2.setId(n2_id)
-            self.m_nodes.append(n2)
-            self.m_graph[node2] = []
-
-        self.m_graph[node1].append((node2, weight))
-        if not self.m_directed:
-            self.m_graph[node2].append((node1, weight))
+        for (current, l) in self.edges.items():
+            out = out + "[" + current + "] -> "
+            for (node, weight) in l:
+                out = out + "(" + node + ", " + str(weight) + ") -> "
+            out = out + "X\n"
+        return out
 
 
-    #############################
-    # devolver nodos
-    ##########################
+    def get_nodes(self):
+        ns = set()
+        for n in self.edges.keys():
+            ns.add(n)
+        return ns
 
-    def getNodes(self):
-        return self.m_nodes
 
-    #######################
-    #    devolver o custo de uma aresta
-    ##############3
+    def add_edge(self, src, dest, weight=0):
+        # src is a new node
+        if src not in self.edges:
+            self.edges[src] = []
+        # dest is a new node
+        if dest not in self.edges:
+            self.edges[dest] = []
 
-    def get_arc_cost(self, node1, node2):
-        custoT = math.inf
-        a = self.m_graph[node1]  # lista de arestas para aquele nodo
-        for (nodo, custo) in a:
-            if nodo == node2:
-                custoT = custo
+        self.edges[src].append((dest, weight))
+        # graph has no direction
+        if not self.directed:
+            self.edges[dest].append((src, weight))
 
-        return custoT
 
-    ##############################
-    #  dado um caminho calcula o seu custo
-    ###############################
+    def get_edge_cost(self, src, dest):
+        for (node, weight) in self.edges[src]:
+            if node == dest:
+                return weight
+        return math.inf
 
-    def calcula_custo(self, caminho):
-        # caminho é uma lista de nodos
-        teste = caminho
-        custo = 0
+
+    def get_neighbours(self, node):
+        return self.edges[node]
+
+
+    def get_path_cost(self, path):
+        cost = 0
         i = 0
-        while i + 1 < len(teste):
-            custo = custo + self.get_arc_cost(teste[i], teste[i + 1])
-            #print(teste[i])
+        while i + 1 < len(path):
+            c = self.get_edge_cost(path[i], path[i + 1])
+            # invalid path
+            if c is math.inf:
+                return math.inf
+
+            cost = cost + c
             i = i + 1
-        return custo
 
-    ################################################################################
-    #     procura DFS
-    ####################################################################################
+        return cost
 
-    def procura_DFS(self, source, destiny, path, visited):
-        visited.add(source)
-        path.append(source)
-
-        if source == destiny:
-            return 0, True
-
-        for (node, w) in self.m_graph[source]:
-            # nodo não visitado
-            if node not in visited:
-                c, f = self.procura_DFS(node, destiny, path, visited)
-                if f:
-                    return w + c, True
-
-        path.pop()
-        return 0, False
-
-
-    #####################################################
-    # Procura BFS
-    ######################################################
-
-    def procura_BFS(self, source, destiny, path, visited):
-        visited.add(source)
-
-        parents = dict()
-        parents[source] = None
-        costs = dict()
-        costs[source] = 0
-
-        queue = list()
-        queue.append(source)
-        found = False
-
-        while len(queue) != 0 and not found:
-            temp = queue.pop(0)
-            for (node, w) in self.m_graph[temp]:
-                # nodo não visitado
-                if node not in visited:
-                    visited.add(node)
-                    parents[node] = temp
-                    costs[node] = costs[temp] + w
-                    if node == destiny:
-                        found = True
-                        break
-                    queue.append(node)
-
-        total = 0
-        # nodo encontrado
-        if found:
-            temp = destiny
-            while parents[temp] is not None:
-                path.append(temp)
-                temp = parents[temp]
-            path.append(source)
-            path.reverse()
-            total = costs[destiny]
-
-        return total, found
-
-
-    ####################
-    # função  getneighbours, devolve vizinhos de um nó
-    ##############################
-
-    def getNeighbours(self, nodo):
-        lista = []
-        for (adjacente, peso) in self.m_graph[nodo]:
-            lista.append((adjacente, peso))
-        return lista
-
-    ###########################
-    # desenha grafo  modo grafico
-    #########################
-
-    def desenha(self):
-        ##criar lista de vertices
-        lista_v = self.m_nodes
-        lista_a = []
+    def draw(self):
         g = nx.Graph()
-        for nodo in lista_v:
-            n = nodo.getName()
-            g.add_node(n)
-            for (adjacente, peso) in self.m_graph[n]:
-                lista = (n, adjacente)
-                # lista_a.append(lista)
-                g.add_edge(n, adjacente, weight=peso)
+        for node in self.edges.keys():
+            g.add_node(node)
+            for (neighbour, w) in self.edges[node]:
+                g.add_edge(node, neighbour, weight=w)
 
         pos = nx.spring_layout(g)
         nx.draw_networkx(g, pos, with_labels=True, font_weight='bold')
@@ -218,9 +86,61 @@ class Graph:
         plt.draw()
         plt.show()
 
-   
 
+    def depth_first_search(self, src, dest, path, visited):
+        visited.add(src)
+        path.append(src)
 
+        if src == dest:
+            return True
 
+        for (node, w) in self.edges[src]:
+            # node not visited
+            if node not in visited:
+                if self.depth_first_search(node, dest, path, visited):
+                    return True
 
+        path.pop()
+        return False
+
+    def breadth_first_search(self, src, dest, path, visited):
+        visited.add(src)
+
+        if src == dest:
+            return True
+
+        found = False
+
+        # queue for nodes to visit
+        q = queue.Queue()
+        q.put(src)
+
+        # dictionary to reconstruct the path
+        parents = dict()
+        parents[src] = None
+
+        while not q.empty() and not found:
+            current = q.get()
+
+            for (node, w) in self.edges[current]:
+                # node not visited
+                if node not in visited:
+                    visited.add(node)
+                    parents[node] = current
+                    if node == dest:
+                        found = True
+                        break
+                    q.put(node)
+
+        # if dest was found, reconstruct the path
+        if found:
+            current = dest
+            while parents[current] is not None:
+                path.append(current)
+                current = parents[current]
+
+            path.append(src)
+            path.reverse()
+
+        return found
 
--- fichas/ficha-01/main.py
+++ fichas/ficha-01/main.py
@@ -1,84 +1,71 @@
 
 from Graph import Graph
-from Node import Node
+
+def menu():
+    print("----- Graphs -----")
+    print("[1] - Show graph")
+    print("[2] - Draw graph")
+    print("[3] - Show nodes")
+    print("[4] - Depth-First Search")
+    print("[5] - Breadth-First Search")
+    print("[0] - Exit")
 
 
 def main():
 
-    #add nodes and connections
+    g1 = Graph(False)
+    g1.add_edge("s", "a", 2)
+    g1.add_edge("a", "b", 2)
+    g1.add_edge("b", "c", 2)
+    g1.add_edge("c", "d", 3)
+    g1.add_edge("d", "t", 3)
+    g1.add_edge("t", "g", 2)
+    g1.add_edge("g", "f", 2)
+    g1.add_edge("f", "e", 5)
+    g1.add_edge("e", "s", 2)
 
-    g = Graph(False)
-    g.add_edge("s", "a", 2)
-    g.add_edge("a", "b", 2)
-    g.add_edge("b", "c", 2)
-    g.add_edge("c", "d", 3)
-    g.add_edge("d", "t", 3)
-    g.add_edge("t", "g", 2)
-    g.add_edge("g", "f", 2)
-    g.add_edge("f", "e", 5)
-    g.add_edge("e", "s", 2)
+    print(g1.edges)
 
-    '''
-    g = Graph(True)
-    g.add_edge("1", "2", 1)
-    g.add_edge("1", "3", 2)
-    g.add_edge("3", "4", 1)
-    g.add_edge("4", "1", 3)
-    g.add_edge("4", "5", 4)
-    g.add_edge("5", "6", 2)
-    g.add_edge("6", "4", 1)
-    g.add_edge("5", "7", 2)
-    '''
+    out = -1
+    while out != 0:
+        menu()
+        out = int(input("option: "))
 
-    saida = -1
-    while saida != 0:
-        print("1-Imprimir Grafo")
-        print("2-Desenhar Grafo")
-        print("3-Imprimir  nodos de Grafo")
-        print("4-Imprimir arestas de Grafo")
-        print("5-DFS")
-        print("6-BFS")
-        print("0-Saír")
-
-        saida = int(input("introduza a sua opcao-> "))
-        if saida == 0:
-            print("saindo.......")
-        elif saida == 1:
-            print(g.m_graph)
-            l = input("prima enter para continuar")
-        elif saida == 2:
-            g.desenha()
-        elif saida == 3:
-            print(g.m_graph.keys())
-            l = input("prima enter para continuar")
-        elif saida == 4:
-            print(g.imprime_aresta())
-            l = input("prima enter para continuar")
-        elif saida == 5:
-            inicio = input("Nodo inicial->")
-            fim = input("Nodo final->")
-            path = []
+        if out == 0:
+            print("leaving...")
+        elif out == 1:
+            print(g1)
+            input("press enter to continue")
+        elif out == 2:
+            g1.draw()
+        elif out == 3:
+            print("nodes: ", g1.get_nodes())
+            input("press enter to continue")
+        elif out == 4:
+            start = input("source node: ")
+            end = input("destiny node: ")
+            path = list()
             visited = set()
-            print(g.procura_DFS(inicio, fim, path, visited))
-            print("path: ")
-            print(path)
-            print("visited: ")
-            print(visited)
-            l = input("prima enter para continuar")
-        elif saida == 6:
-            inicio = input("Nodo inicial->")
-            fim = input("Nodo final->")
-            path = []
+
+            print("found: ", g1.depth_first_search(start, end, path, visited))
+            print("path: ", path)
+            print("path cost: ", g1.get_path_cost(path))
+            print("visited: ", visited)
+            input("press enter to continue")
+        elif out == 5:
+            start = input("source node: ")
+            end = input("destiny node: ")
+            path = list()
             visited = set()
-            print("Encontrou: ", g.procura_BFS(inicio, fim, path, visited))
-            print("path: ")
-            print(path)
-            print("visited: ")
-            print(visited)
-            l = input("prima enter para continuar")
+
+            print("found: ", g1.breadth_first_search(start, end, path, visited))
+            print("path: ", path)
+            print("path cost: ", g1.get_path_cost(path))
+            print("visited: ", visited)
+            input("press enter to continue")
         else:
-            print("you didn't add anything")
-            l = input("prima enter para continuar")
+            print("invalid choice!")
+            input("press enter to continue")
 
 
 if __name__ == "__main__":
